f0d19817b2c617c4370f5faac10b9ae9ea019f78
Holger Cremer <HolgerCremer@gmail.com>
2015-10-03 17:28:10 +0200

    Hide pads in the pad list if: - the name starts with private_ - the revision is 0

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ A plugin for Etherpad-lite that shows a list of all pads sorted by last edit dat
     npm install ep_pad-lister (from your etherpad-lite folder)
 
 
-Put the follwing snippet into ```src/static/custom/index.csss```
+Put the follwing snippet into ```src/static/custom/index.css```
 
 ```
 .pad-lister-link-container {
   font-size: 16px;
-  width: 350px;
+  max-width: 500px;
   text-align:center;
   min-height: 20px;
   padding: 19px;
@@ -21,5 +21,11 @@ Put the follwing snippet into ```src/static/custom/index.csss```
   border: 1px solid #e3e3e3;
   border-radius: 4px;
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);
+}
+.pad-lister-link-container .code {
+  font-family: monospace; 
+  padding: 0 5px;
+  color: #c7254e; 
+  background-color: #fff;
 }
 ```
--- index.js
+++ index.js
@@ -4,13 +4,17 @@ var eejs = require('ep_etherpad-lite/node/eejs');
 var padManager = require('ep_etherpad-lite/node/db/PadManager');
 var dateFormat = require('dateformat');
 
+var PRIVATE_PAD_PREFIX = 'private_';
+
 exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_pad-lister/templates/linkToList.ejs");
+  var render_args = {
+    PRIVATE_PAD_PREFIX: PRIVATE_PAD_PREFIX
+  };
+  args.content = args.content + eejs.require("ep_pad-lister/templates/linkToList.ejs", render_args);
   return cb();
 };
 
 exports.registerRoute = function (hook_name, args, cb) {
-  var h = '<b>test</b>'
   args.app.get('/pad-lister/static/bootstrap.min.css', function (req, res) {
     res.sendfile(__dirname + '/static/css/bootstrap.min.css');
   });
@@ -18,6 +22,7 @@ exports.registerRoute = function (hook_name, args, cb) {
   args.app.get('/pad-lister', function (req, res) {
     getDetailedPadList(function (pads) {
       var render_args = {
+        PRIVATE_PAD_PREFIX: PRIVATE_PAD_PREFIX,
         pads: pads
       };
       res.send(eejs.require('ep_pad-lister/templates/list.html', render_args));
@@ -26,12 +31,45 @@ exports.registerRoute = function (hook_name, args, cb) {
 };
 
 function getDetailedPadList(callback) {
+
+  var padsToDo;
+  var padData = [];
+
+  function doneAction() {
+    padsToDo--;
+    if (padsToDo > 0) {
+      return;
+    }
+    // sort
+    padData = sortPadData(padData);
+    // format each timestamp
+    padData.forEach(function (padObj) {
+      padObj.lastAccess = formatDate(padObj.lastAccess);
+    });
+    callback(padData);
+  }
+
   padManager.listAllPads(function (err, listResult) {
     var padNames = listResult.padIDs;
+    // if we have no pads...
+    if (padNames.length === 0) {
+      callback([]);
+      return;
+    }
 
-    var padData = [];
+    padsToDo = padNames.length;
     padNames.forEach(function (padName) {
+      // ignore private pads
+      if (padName.indexOf(PRIVATE_PAD_PREFIX) === 0) {
+        doneAction();
+        return;
+      }
       padManager.getPad(padName, function (err, pad) {
+        // ignore pads without any changes
+        if (pad.head === 0) {
+          doneAction();
+          return;
+        }
         pad.getLastEdit(function (err, time) {
           padData.push({
             name: padName,
@@ -39,15 +77,7 @@ function getDetailedPadList(callback) {
             lastAccess: time
           });
 
-          if (padData.length === padNames.length) {
-            // sort
-            padData = sortPadData(padData);
-            // format each timestamp
-            padData.forEach(function (padObj) {
-              padObj.lastAccess = formatDate(padObj.lastAccess);
-            });
-            callback(padData);
-          }
+          doneAction();
         });
       }); // end getPad
     }); // end forEach
